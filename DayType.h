796ee1ceea40a2ceaa1ed40795b8e0915c8032f9
JoshToppvpcc <JoshuaTopp47@gmail.com>
2024-04-05 16:10:32 -0400

    ok ok, now its done

--- DayType.h
+++ DayType.h
@@ -3,27 +3,36 @@
 #include <string>
 
 using namespace std;
+//Class Daytype, used to set a day of the week and find any related days, wether it be the next day, previous day, or what day it is 50 days later. 
 class DayType
 {
 private: 
 	string weekDay;
 public:
 	string weekDays[7];
-
+	//Default constructor, sets the value of weekDay to Sunday. 
 	DayType();
 
+	//Postcondition- Takes the input of the day, string d, and sets it as the weekday for the entire prgram. 
 	DayType(string d);
 
+	//Postcondition- Sets the string d equal to weekDay. 
 	void setDay(string d);
 
+	//Returns the value of weekDay which should be the string d given, which is the given day of the week. Makes it easy to cout the selected day of the week. 
 	string getDay() const;
 
+	//similar to getDay(), calling this function simply prints the value of weekDay so long as it has been set in DayType and setDay. 
 	void print() const;
 
+	//Postcondition- Takes the given weekDay and returns the value of the next day in the calendar week.
 	string nextDay() const;
 
+	//Postcondition- takes the given weekDay and returns the value of the previous day. 
 	string prevDay() const;
 
+	//Postcondition- The function can take an input of nDays (number of days to be added) and correctly add them to the current weekDay set in the previous functions.
+	// This function can add any amount of days. 
 	void addDay(int nDays);
 
 
--- DayTypeClass2JoshuaTopp.cpp
+++ DayTypeClass2JoshuaTopp.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include "DayType.h"
 using namespace std;
-
+//Assigning the values to each spot in the weekDays array. Default constructor.
 DayType::DayType() 
 {
 	weekDay = "blank";
@@ -16,6 +16,7 @@ DayType::DayType()
 	weekDays[5] = "Friday";
 	weekDays[6] = "Saturday";
 }
+//Constructor with parameters, also sets the values for the array but can assign an input to d, setting a specific weekDay to the entire program. 
 DayType::DayType(string d)
 {
 	weekDay = d;
@@ -27,20 +28,26 @@ DayType::DayType(string d)
 	weekDays[5] = "Friday";
 	weekDays[6] = "Saturday";
 }
+//Sets the variable weekDay equal to the string input in the arguments spot in setDay(). 
 void DayType::setDay(string d) 
 {
 	weekDay = d;
 }
 
+//A function to return the value of weekDay. Can be useful when checking what weekDay has been assigned to. 
 string DayType::getDay() const
 {
 	return weekDay;
 }
+//similar to the getDay() function, calling this function will print the value of weekDay. 
 void DayType::print() const 
 {
 	cout << weekDay;
 }
-
+//creates a variable called index to run through the array, see which day is equal to the one given, or weekDay,
+//  then returns the value of the index + 1 to return the next day.
+//  If only this if statement was here and weekDay = weekDays[6], adding one would give weekDays[7] which does not exist in this array. 
+//I had to add a special case where if weekDay = weekDays[6], it would return weekDays[0] which is the next day in a calendar week. 
 string DayType::nextDay() const
 {
 	int index = 0;
@@ -60,6 +67,11 @@ string DayType::nextDay() const
 		return weekDays[index + 1];
 	}
 }
+//This is a similar set up to the nextDay() function, but the other way around. 
+// The variabe index is created to run through the array, find the value that equals the given weekDay,
+//  and subtract one from its position in the array to get the new value.
+//  Similarly,there is a special case where, if weekDay=weekDays[0], subtracting one would give weekDays[-1] which does not exist,
+//  so I had to make sure it equalled weekDays[6] which is the previous day in the calendar week. 
 string DayType::prevDay() const 
 {
 	int index = 0;
@@ -81,7 +93,9 @@ string DayType::prevDay() const
 
 }
 
-
+//This function takes the weekDay value, and adds to it the value of nDays which is given as an argument when the fucntion is called. 
+// Since this arry of weekDays only has 7 values, I had to take the value of nDays and modulo it with 7, to get the remainder. 
+// This would automatically elimante the number of exact weeks that would pass, as only the remainder would change the value of the day.
 void DayType::addDay(int nDays)
 {
 	int num = nDays % 7; 
@@ -103,13 +117,19 @@ void DayType::addDay(int nDays)
 }
 
 
-
+//The main function starts with assigning value to the weekDay, by creating object "today" of class "DayType"
+//It then prints out the correct information because all the functions are running properly. 
+//It starts with the default constructor and it prints out its value plainly.
+//  Then it calls the nextDay and prevDay functions to do their part in finding the value of the next and previous day of the default constructor "Sunday".
+//Then once that runs successfully, it moves on to the object I assigned, which is "Monday". 
+// It then finds the next and previous day of Monday, then it adds 3 days to Monday and outputs it.
+//Then it adds 30 and 365 days to Monday and outputs it. 
 int main()
 {
 
 	
-	DayType today("Thursday");
-	DayType defaultDay("sunday");
+	DayType today("Monday");
+	DayType defaultDay("Sunday");
 	cout << "From default constructor : " << defaultDay.getDay() << endl;
 	cout << "The previous day is " << defaultDay.prevDay() << endl;
 	cout << "The day after is " << defaultDay.nextDay() << endl;
